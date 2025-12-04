0bdc0392a0b2569a902301490242858b0f3ccc5b
0y <0yqc@duck.com>
2025-12-04 21:57:13 +0100

    finished CardGamePerfect. It calculates the minimum ammount of cards needed for up to n players, so all cards can always be handed out evenly

--- src/CardGamePerfect.kt
+++ src/CardGamePerfect.kt
@@ -1,10 +1,6 @@
 fun kgv(vararg nums: Int): Int { // kgV is german for Kleinstes gemeinsames Vielfaches (Least common multiple)
 	val max_num = nums.maxOrNull() ?: error("no numbers")
-	var max_kgv = 1
-	for (num: Int in nums) {
-		max_kgv *= num
-	}
-	for (currentKgv: Int in max_num .. max_kgv step max_num) {
+	for (currentKgv: Int in max_num .. Int.MAX_VALUE step max_num) {
 		var possible = true
 		for (num: Int in nums) {
 			if (currentKgv % num != 0) {
@@ -19,5 +15,7 @@ fun kgv(vararg nums: Int): Int { // kgV is german for Kleinstes gemeinsames Viel
 }
 
 fun main() {
-	println(kgv(2, 6))
+	for (i in 2..10) {
+		println("up to $i players: ${kgv(*(2 .. i).toList().toIntArray())}")
+	}
 }
